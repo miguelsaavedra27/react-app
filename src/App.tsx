32a109a19790114ef25b2589ac23fc5f3b7ee5f1
Miguel Saavedra <miguel.saavedra@uc.cl>
2023-09-13 12:28:51 -0300

    Passing function via props

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
 import ListGroup from "./components/ListGroup";
-function App(){
-  return <div><ListGroup /></div>;
+function App() {
+  let cities = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+  return (
+    <div>
+      <ListGroup
+        cities={cities}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
+    </div>
+  );
 }
 export default App;
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,8 +1,15 @@
 import { useState } from "react";
 
-function ListGroup() {
-  let cities = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+// {cities: [], heading: string}
 
+interface ListGroupProps {
+  cities: string[];
+  heading: string;
+  // (item: string) => void
+  onSelectItem: (item: string) => void;
+}
+
+function ListGroup({ cities, heading, onSelectItem }: ListGroupProps) {
   // Hook is a function that allows us to add state to a functional component. Using the useState hook, we can add state to a functional component.
   // This returns an array with two elements. The first element is the current value of the state, and the second element is a function that allows us to update the state.
   const [selectedIndex, setSelectedIndex] = useState(-1); // This is called array destructuring. We can use this to get the first and second element of the array.
@@ -11,7 +18,7 @@ function ListGroup() {
   // With bracets we can use JS inside the return statement.
   return (
     <>
-      <h1>My List Group</h1>
+      <h1>{heading}</h1>
       {cities.length === 0 && <p>There are no cities to render.</p>}{" "}
       {/* This is a common practice that React developers use to render content dynamically. */}
       <ul className="list-group">
@@ -23,7 +30,10 @@ function ListGroup() {
                 ? "list-group-item active" // If the index is equal to the selected index, then add the active class.
                 : "list-group-item" // Otherwise, add the default class.
             }
-            onClick={() => setSelectedIndex(index)} // These return SyntheticBaseEvent object, which is a React object.
+            onClick={() => {
+              setSelectedIndex(index)
+              onSelectItem(city);}
+            }
           >
             {city}
           </li>
