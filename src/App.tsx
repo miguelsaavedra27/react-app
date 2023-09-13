ffad97da7654d67c545f86df74bfbc56996476a2
Miguel Saavedra <miguel.saavedra@uc.cl>
2023-09-13 12:42:50 -0300

    Passing children

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
+
 function App() {
-  let cities = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
   return (
     <div>
-      <ListGroup
-        cities={cities}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />
+      <Alert>
+        <h1>Alert Title</h1>
+        <p>Alert Body</p>
+      </Alert>
     </div>
   );
 }
