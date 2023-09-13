697b45f756421f265f6ff8dc8c57bba73e55c832
Miguel Saavedra <miguel.saavedra@uc.cl>
2023-09-13 11:22:40 -0300

    onClick

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,17 +1,22 @@
 function ListGroup() {
   let cities = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
 
-  cities = [];
+  // cities = [];
 
   // In JSX we don't have for loops, we can use map instead.
   // With bracets we can use JS inside the return statement.
   return (
     <>
       <h1>My List Group</h1>
-      {cities.length === 0 && <p>There are no cities to render.</p>} {/* This is a common practice that React developers use to render content dynamically. */}
+      {cities.length === 0 && <p>There are no cities to render.</p>}{" "}
+      {/* This is a common practice that React developers use to render content dynamically. */}
       <ul className="list-group">
         {cities.map((city, index) => (
-          <li key={index} className="list-group-item">
+          <li
+            key={index}
+            className="list-group-item"
+            onClick={() => console.log(city)}
+          >
             {city}
           </li>
         ))}
