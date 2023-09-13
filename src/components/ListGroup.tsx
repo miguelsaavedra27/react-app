61393884f62ce32bc5e05c286ae9e9ad8c674132
Miguel Saavedra <miguel.saavedra@uc.cl>
2023-09-13 11:47:37 -0300

    selectedIndex

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,10 +1,11 @@
-import { MouseEvent } from "react";
+import { useState } from "react";
 
 function ListGroup() {
   let cities = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
 
-  // Event handler. Handles the click event.
-  const handleClick = (event: MouseEvent) => console.log(event);
+  // Hook is a function that allows us to add state to a functional component. Using the useState hook, we can add state to a functional component.
+  // This returns an array with two elements. The first element is the current value of the state, and the second element is a function that allows us to update the state.
+  const [selectedIndex, setSelectedIndex] = useState(-1); // This is called array destructuring. We can use this to get the first and second element of the array.
 
   // In JSX we don't have for loops, we can use map instead.
   // With bracets we can use JS inside the return statement.
@@ -17,8 +18,12 @@ function ListGroup() {
         {cities.map((city, index) => (
           <li
             key={index}
-            className="list-group-item"
-            onClick={handleClick} // These return SyntheticBaseEvent object, which is a React object.
+            className={
+              index === selectedIndex
+                ? "list-group-item active" // If the index is equal to the selected index, then add the active class.
+                : "list-group-item" // Otherwise, add the default class.
+            }
+            onClick={() => setSelectedIndex(index)} // These return SyntheticBaseEvent object, which is a React object.
           >
             {city}
           </li>
