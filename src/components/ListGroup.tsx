0cba3f8abb781f1c46187f4f37b97b5f33985965
Miguel Saavedra <miguel.saavedra@uc.cl>
2023-09-13 10:48:30 -0300

    fragment

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,19 @@
 function ListGroup() {
   return (
-    <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
+    <>
+        <h1>My List Group</h1>
+        <ul className="list-group">
+          <li className="list-group-item">An item</li>
+          <li className="list-group-item">A second item</li>
+          <li className="list-group-item">A third item</li>
+          <li className="list-group-item">A fourth item</li>
+          <li className="list-group-item">And a fifth one</li>
+        </ul>
+    </>
   );
 }
 
 export default ListGroup;
+
+// In react, a component can't return more than one element.
+// To solve this, we can wrap the elements in a div or a fragment to wrap all the children.
